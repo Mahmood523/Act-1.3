5aa4e162573e5521516e88644a060c38b7de456a
Mahmood523 <91906977+Mahmood523@users.noreply.github.com>
2021-10-07 22:35:23 +0200

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,32 +1,35 @@
 Activité 1.2.4 : Est-ce un palindrome ?
 
-fonction palindrome (ref Mot :chaine de caracteres) :booléen;
-  var resultat :booléen;
-      i,n :entiers;
+import java.util.Scanner;
+public class Palindrome {
 
-  debut 
-      fonction saisie(Mot :chaine de caractere):vide;
-          ecrire("Saisir votre mot :");
-          lire(Mot);
-      finfonction;
-    
-      n<- longueur(mot);
-      resultat<- vrai;
-      i<- 0;
+     static boolean estPalindrome(String mot) 
+     {
+       int i=0, longueur=mot.length()-1;
+       boolean egale=true;
+   
+        while(i<longueur/2 && egale){
+           if(mot.charAt(i)==mot.charAt(longueur-i))
+              egale = true;
+           else
+              egale = false;
+           i++;
+     }
+        return egale;
+}
 
-      tant que resultat= vrai et i <= n/2 faire
-
-        si (Mot[i] ≠ Mot [n-1-i] ) alors
-          resultat<- faux;
-        finsi
-
-        i<- i+1
-      fintantque 
-
-    si resultat= vrai alors
-     ecrire("votre mot est palindrome")
-    sinon ecrire("votre mot n'est palindrome")
-
-  fin 
-finfonction
+public static void main(String a[])
+{
+      String mot, mot_inverse = "";
+       Scanner in = new Scanner(System.in);
+  
+       System.out.println("Entrer un mot ");
+       mot = in.nextLine();
+  
+       if (estPalindrome(mot))
+          System.out.println("'"+mot + "' est un palindrome");
+       else
+          System.out.println("'"+mot + "' n'est pas un palindrome");
+}
+}
  
