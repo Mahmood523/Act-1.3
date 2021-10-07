6e461efb3203d39f12c61e0a5d077850ec2f9971
Mahmood523 <91906977+Mahmood523@users.noreply.github.com>
2021-10-08 00:52:21 +0200

    Update EpeesCollegues.java

--- EpeesCollegues.java
+++ EpeesCollegues.java
@@ -50,5 +50,55 @@ public static void main(String[] args)
         displayTab(prenom);
 }
 }
+// avec 1ere methode de parcours
 
+public class EpeesCollegues {
+
+static void tri_bulle(String[] prenom) 
+{
+        int taille = prenom.length;  
+        String tmp = "0";
+ 
+        for(int i=taille; i > 0; i--) 
+        {
+                for(int j=taille-1; j > 1; j--)
+                {  
+                        if(prenom[j].compareTo (prenom[j-1] ) < 0)
+                        {
+                                //echanges des elements
+                                tmp = prenom[j-1];  
+                                prenom[j-1] = prenom[j];  
+                                prenom[j] = tmp;  
+                        }
+ 
+                }
+                       
+        }
+}
+
+static void displayTab(String[] prenom)
+   {    
+        for(int i=0; i < prenom.length; i++)
+        {
+                System.out.print(prenom[i] + " ");  
+        }
+        System.out.println();
+    }
+
+public static void main(String[] args)
+{
+        String[] prenom = {"Alban", "Jim", "Bob", "Albatroz", "Jinto", "Bow", "Hijo", "Manu", "Seb", "Teilo", "Charles", "Xavier"};
+        System.out.println("---Avant le tri a bulle---"); 
+    
+        displayTab(prenom);
+ 
+        // tri des elements de tableau avec le tri a bulle
+        tri_bulle(prenom);
+ 
+        System.out.println("---Apres le tri a bulle---");  
+    
+        displayTab(prenom);
+}
+}
+// avec 2eme methode de parcours
     
